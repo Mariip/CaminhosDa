6ff881022b6291bdeea768c5c418ba1ff9c9dc4f
Mariip <marinovaes93@gmail.com>
2015-06-01 16:35:36 -0300

    cssfix2

--- header.php
+++ header.php
@@ -27,15 +27,18 @@
 <style type="text/css">
 <?php if ( $cor_secundaria != "" ) : ?>
 
-    body, .quadradodatahoje, #filtros li,.hide, .entry-header-hoje2,  #legenda, .entry-header .finalizado, .topo .finalizado,.entry-header3 .finalizado,.quadradodatahoje, #site-navigation, .datanoticiahoje, .singlem,  #site-navigation li, .evento .datanoticiahoje hr, .entry-header2 {
+    body, .quadradodatahoje, #filtros li,.hide, .entry-header-hoje2,  #legenda, .entry-header .finalizado, .topo .finalizado,.entry-header3 .finalizado,.quadradodatahoje, .evento, #site-navigation, .datanoticiahoje, .singlem,  #site-navigation li, .evento .datanoticiahoje hr, .entry-header2 {
         background: <?php echo $cor_secundaria;?>;
         
     }
     .texto-secundario, .evento .datanoticiahoje, h2,#calendario li a:hover, .quadradodata, .info > h1, #content header, #proximos, #content a:hover, #content a:focus {
         color: <?php echo $cor_secundaria ;?>;
     }
-    .single article,.page article, blockquote ,.fb-like {
+    .single article,.page article, blockquote ,.fb-like, #calendario li a:hover {
         border-color: <?php echo $cor_secundaria; ?>;
+    }
+    #calendario li a:hover {
+        box-shadow: 0 0 0pt 3pt <?php echo $cor_fundo; ?>;    
     }
     #calendario li{
         box-shadow: 0 0 0pt 2pt <?php echo $cor_principal; ?>;
@@ -50,7 +53,7 @@
         border-color: <?php echo $cor_principal; ?>;
     }
 
-    .page-template-search-php .topo, .search .topo, #calendario li, #searchbox ,.numero, .topo h2, #nav-below, #search, .search, .search-results, .facebook, .evento, .faixa, #menu, #site-info, .hentry, .entry-header, .entry-header3 , hr, .entry-header-hoje {
+    .page-template-search-php .topo, .search .topo, #calendario li, #searchbox ,.numero, .topo h2, #nav-below, #search, .search, .search-results, .facebook, .faixa, #menu, #site-info, .hentry, .entry-header, .entry-header3 , hr, .entry-header-hoje {
         background: <?php echo $cor_principal; ?>;
     }
     #calendario li a:hover,#filtros li .selecionado,.entry-header2 .finalizado , .nav-previous, .nav-next, .page-template-search-php hr, #searchmenu hr, .search hr, #site-navigation li a:hover, .datanoticiahoje hr , #primary, .evento hr, .dataslider-up hr, .evento .datanoticiahoje, .quadradodata, .twitter, .site-title, .event-date, .wp-caption-text, #filtros li a:active, #filtros li a:hover, #site-navigation li:first-of-type {
